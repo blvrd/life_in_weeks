8790f49b4e798e4a83fcf217c3902552893692a1
Garrett Martin <me@garrettqmartin.com>
2016-04-10 14:38:27 -0500

    Add date input for dynamic date.

--- weeks.js
+++ weeks.js
@@ -1,13 +1,34 @@
 var container = document.getElementById("weeks");
-var birthDate = new Date("1994", "05", "29");
+var birthDate = null
 var currentDate = new Date();
-var weeksAlive = parseInt((currentDate-birthDate)/(1000*60*60*24*7).toFixed())
+var weeksLifespan = 4693 // 90 years
+var dateInput = document.getElementById("dob")
 
-for (i = 0; i < 4693; i++) {
-  var weekBlock = document.createElement("li")
-  weekBlock.className = "week"
-  if (i <= weeksAlive) {
-    weekBlock.className += " active"
-  }
-  container.appendChild(weekBlock)
+var calculateWeeksAlive = function() {
+  window.weeksAlive = parseInt((currentDate-birthDate)/(1000*60*60*24*7).toFixed())
 }
+
+var setDate = function(e) {
+  window.birthDate = new Date(e.target.value)
+  calculateWeeksAlive()
+  generateWeeks()
+}
+
+dateInput.onchange = setDate
+
+var generateWeeks = function() {
+  while(container.hasChildNodes()){
+    container.removeChild(container.lastChild);
+  }
+
+  for (i = 0; i < weeksLifespan; i++) {
+    var weekBlock = document.createElement("li")
+    weekBlock.className = "week"
+    if (i <= weeksAlive) {
+      weekBlock.className += " active"
+    }
+    container.appendChild(weekBlock)
+  }
+}
+
+generateWeeks()
