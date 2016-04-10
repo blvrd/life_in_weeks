3bdc190515c54e0c60af83a23dd551e35514d586
Garrett Martin <me@garrettqmartin.com>
2016-04-10 17:27:31 -0500

    Use purer functions.

--- weeks.js
+++ weeks.js
@@ -1,34 +1,49 @@
-var container = document.getElementById("weeks");
-var birthDate = null
-var currentDate = new Date();
-var weeksLifespan = 4693 // 90 years
-var dateInput = document.getElementById("dob")
-
-var calculateWeeksAlive = function() {
-  window.weeksAlive = parseInt((currentDate-birthDate)/(1000*60*60*24*7).toFixed())
+function getCookie (cookieName, callback){
+    chrome.cookies.get({
+      'url':'http://www.google.com',
+      'name':cookieName
+    },
+    function(data){
+        callback(data);
+    });
 }
 
-var setDate = function(e) {
-  window.birthDate = new Date(e.target.value)
-  calculateWeeksAlive()
-  generateWeeks()
+window.container = document.getElementById("weeks");
+getCookie("birthDate", function(cookie) {
+  generateWeeks(new Date(cookie.value))
+})
+window.currentDate = new Date();
+window.weeksLifespan = 4693 // 90 years
+window.dateInput = document.getElementById("dob")
+
+var calculateWeeksAlive = function(birthDate) {
+  if (birthDate.toString().length > 1) {
+    return parseInt((window.currentDate-birthDate)/(1000*60*60*24*7).toFixed())
+  }
 }
 
-dateInput.onchange = setDate
-
-var generateWeeks = function() {
+var generateWeeks = function(birthDate) {
   while(container.hasChildNodes()){
     container.removeChild(container.lastChild);
   }
 
+  var weeksAlive = calculateWeeksAlive(birthDate)
+
   for (i = 0; i < weeksLifespan; i++) {
     var weekBlock = document.createElement("li")
     weekBlock.className = "week"
-    if (i <= weeksAlive) {
+    if (weeksAlive && i <= weeksAlive) {
       weekBlock.className += " active"
     }
     container.appendChild(weekBlock)
   }
 }
 
-generateWeeks()
+var setDate = function(e) {
+  var birthDate = new Date(e.target.value)
+  chrome.cookies.set({ url: 'http://www.google.com', name: 'birthDate', value: birthDate.toString() })
+  generateWeeks(birthDate)
+}
+
+dateInput.onchange = setDate
+
