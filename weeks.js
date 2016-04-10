ec2909743b7d2d7e913f9ddbea1cd0f4bac30d9e
Garrett Martin <me@garrettqmartin.com>
2016-04-10 14:12:07 -0500

    Add weeksAlive calculation.

--- weeks.js
+++ weeks.js
@@ -1,9 +1,12 @@
-var container = document.getElementById("weeks")
+var container = document.getElementById("weeks");
+var birthDate = new Date("1994", "05", "29");
+var currentDate = new Date();
+var weeksAlive = parseInt((currentDate-birthDate)/(1000*60*60*24*7).toFixed())
 
 for (i = 0; i < 4693; i++) {
   var weekBlock = document.createElement("li")
   weekBlock.className = "week"
-  if (i <= 1095) {
+  if (i <= weeksAlive) {
     weekBlock.className += " active"
   }
   container.appendChild(weekBlock)
